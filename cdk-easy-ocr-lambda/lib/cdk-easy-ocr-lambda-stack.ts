46c11f57f50ead9f26d11b42fd15f10e1212f527
ksdyb.park <ksdyb@amazon.com>
2024-02-26 17:31:57 +0900

    update

---
+++ cdk-easy-ocr-lambda/lib/cdk-easy-ocr-lambda-stack.ts
@@ -0,0 +1,208 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as path from "path";
+import * as logs from "aws-cdk-lib/aws-logs"
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as cloudFront from 'aws-cdk-lib/aws-cloudfront';
+import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+
+const region = process.env.CDK_DEFAULT_REGION;    
+const debug = false;
+const stage = 'dev';
+const s3_prefix = 'docs';
+const projectName = `korean-ocr`; 
+const bucketName = `storage-for-${projectName}-${region}`; 
+
+export class CdkEasyOcrLambdaStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // s3 
+    const s3Bucket = new s3.Bucket(this, `storage-${projectName}`,{
+      bucketName: bucketName,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+      publicReadAccess: false,
+      versioned: false,
+      cors: [
+        {
+          allowedHeaders: ['*'],
+          allowedMethods: [
+            s3.HttpMethods.POST,
+            s3.HttpMethods.PUT,
+          ],
+          allowedOrigins: ['*'],
+        },
+      ],
+    });
+    if(debug) {
+      new cdk.CfnOutput(this, 'bucketName', {
+        value: s3Bucket.bucketName,
+        description: 'The nmae of bucket',
+      });
+      new cdk.CfnOutput(this, 's3Arn', {
+        value: s3Bucket.bucketArn,
+        description: 'The arn of s3',
+      });
+      new cdk.CfnOutput(this, 's3Path', {
+        value: 's3://'+s3Bucket.bucketName,
+        description: 'The path of s3',
+      });
+    }
+
+    new cdk.CfnOutput(this, 'HtmlUpdateCommend', {
+      value: 'aws s3 cp ../html/ ' + 's3://' + s3Bucket.bucketName + '/ --recursive',
+      description: 'copy commend for web pages',
+    });
+
+    // cloudfront
+    const distribution = new cloudFront.Distribution(this, `cloudfront-for-${projectName}`, {
+      defaultBehavior: {
+        origin: new origins.S3Origin(s3Bucket),
+        allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,
+        cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+        viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+      },
+      priceClass: cloudFront.PriceClass.PRICE_CLASS_200,  
+    });
+    new cdk.CfnOutput(this, `distributionDomainName-for-${projectName}`, {
+      value: distribution.domainName,
+      description: 'The domain name of the Distribution',
+    });
+
+    new cdk.CfnOutput(this, `WebUrl-for-${projectName}`, {
+      value: 'https://'+distribution.domainName+'/index.html',      
+      description: 'The web url of request for chat',
+    }); 
+
+    // api role
+    const role = new iam.Role(this, `api-role-for-${projectName}`, {
+      roleName: `api-role-for-${projectName}-${region}`,
+      assumedBy: new iam.ServicePrincipal("apigateway.amazonaws.com")
+    });
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: ['*'],
+      actions: [
+        'lambda:InvokeFunction',
+        'cloudwatch:*'
+      ]
+    }));
+    role.addManagedPolicy({
+      managedPolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaExecute',
+    }); 
+
+    // API Gateway
+    const api = new apiGateway.RestApi(this, `api-chatbot-for-${projectName}`, {
+      description: 'API Gateway for ocr',
+      endpointTypes: [apiGateway.EndpointType.REGIONAL],
+      binaryMediaTypes: ['application/pdf', 'text/plain', 'text/csv', 'application/vnd.ms-powerpoint', 'application/vnd.ms-excel', 'application/msword'], 
+      deployOptions: {
+        stageName: stage,
+
+        // logging for debug
+        // loggingLevel: apiGateway.MethodLoggingLevel.INFO, 
+        // dataTraceEnabled: true,
+      },
+    });  
+
+    // lambda - ocr
+    const lambdaEasyOCR = new lambda.Function(this, `lambda-for-${projectName}`, {
+      description: 'lambda for easy ocr',
+      functionName: `lambda-for-${projectName}`,
+      handler: 'lambda_function.lambda_handler',
+      runtime: lambda.Runtime.PYTHON_3_11,
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../lambda-easy-ocr')),
+      timeout: cdk.Duration.seconds(60),
+      logRetention: logs.RetentionDays.ONE_DAY,
+      environment: {
+      }
+    });
+
+    // POST method - ocr
+    const resourceNameOcr = "ocr";
+    const ocr = api.root.addResource(resourceNameOcr);
+    ocr.addMethod('POST', new apiGateway.LambdaIntegration(lambdaEasyOCR, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [  
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+    if(debug) {
+      new cdk.CfnOutput(this, `ApiGatewayUrl-for-${projectName}`, {
+        value: api.url+'ocr',
+        description: 'The url of API Gateway',
+      }); 
+    }
+
+    // cloudfront setting  
+    distribution.addBehavior("/ocr", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
+
+    // Lambda - Upload
+    const lambdaUpload = new lambda.Function(this, `lambda-upload-for-${projectName}`, {
+      runtime: lambda.Runtime.NODEJS_16_X, 
+      functionName: `lambda-upload-for-${projectName}`,
+      code: lambda.Code.fromAsset("../lambda-upload"), 
+      handler: "index.handler", 
+      timeout: cdk.Duration.seconds(10),
+      logRetention: logs.RetentionDays.ONE_DAY,
+      environment: {
+        bucketName: s3Bucket.bucketName,
+        s3_prefix:  s3_prefix
+      }      
+    });
+    s3Bucket.grantReadWrite(lambdaUpload);
+    
+    // POST method - upload
+    const resourceNameUpload = "upload";
+    const upload = api.root.addResource(resourceNameUpload);
+    upload.addMethod('POST', new apiGateway.LambdaIntegration(lambdaUpload, {
+      passthroughBehavior: apiGateway.PassthroughBehavior.WHEN_NO_TEMPLATES,
+      credentialsRole: role,
+      integrationResponses: [{
+        statusCode: '200',
+      }], 
+      proxy:false, 
+    }), {
+      methodResponses: [  
+        {
+          statusCode: '200',
+          responseModels: {
+            'application/json': apiGateway.Model.EMPTY_MODEL,
+          }, 
+        }
+      ]
+    }); 
+    if(debug) {
+      new cdk.CfnOutput(this, `ApiGatewayUrl-for-${projectName}`, {
+        value: api.url+'upload',
+        description: 'The url of API Gateway',
+      }); 
+    }
+
+    // cloudfront setting  
+    distribution.addBehavior("/upload", new origins.RestApiOrigin(api), {
+      cachePolicy: cloudFront.CachePolicy.CACHING_DISABLED,
+      allowedMethods: cloudFront.AllowedMethods.ALLOW_ALL,  
+      viewerProtocolPolicy: cloudFront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+    });    
+  }
+}
